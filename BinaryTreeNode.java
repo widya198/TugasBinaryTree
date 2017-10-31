01b270befdb36b1e9c4b941c362a41b7b41ab71e
widya198 <widiangrm@gmail.com>
2017-10-31 22:18:08 +0700

    Add files via upload

---
+++ BinaryTreeNode.java
@@ -0,0 +1,43 @@
+package BinaryTree;
+
+ public class BinaryTreeNode{
+     
+     BinaryTreeNode left, right;
+     Object element;
+ 
+     public BinaryTreeNode(){
+         left = null;
+         right = null;
+         element = null;
+     }
+
+     public BinaryTreeNode(Object n){
+         left = null;
+         right = null;
+         element = n;
+     }
+
+     public void setLeft(BinaryTreeNode n){
+         left = n;
+     }
+
+     public void setRight(BinaryTreeNode n){
+         right = n;
+     }
+
+     public BinaryTreeNode getLeft(){
+         return left;
+     }
+
+     public BinaryTreeNode getRight(){
+         return right;
+     }
+
+     public void setData(Object d){
+         element = d;
+     }
+
+     public Object getData(){
+         return element;
+     }     
+ }
